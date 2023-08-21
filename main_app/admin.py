3f0b9babe6d016a9eae059eb7641ea728a8fe29e
whuang214 <whuang2146@gmail.com>
2023-08-21 15:39:03 -0400

    registered models in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -4,5 +4,6 @@ from .models import TravelItinerary, Activity, Flight
 # Register your models here.
 
 admin.site.register(TravelItinerary)
+
 admin.site.register(Activity)
 admin.site.register(Flight)
