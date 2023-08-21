6176fe733a4f2c055e9704f3b498ef09af697030
parsachow <parsakishwa06@gmail.com>
2023-08-21 10:28:50 -0400

    first model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class TravelItinerary(models.Model):
+    users = models.ManyToManyField(User)
+    title = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField
+    location = models.CharField(max_length=100)
+    notes = models.TextField()
