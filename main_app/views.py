d6308b47fe98fb30d8c7c4bab72d348b81b6e70e
whuang214 <whuang2146@gmail.com>
2023-08-21 11:44:23 -0400

    add index for itinerary

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import TravelItinerary
+
 
 # Create your views here.
 def home(request):
@@ -6,3 +8,19 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+# GET request will render the template
+class ItineraryIndex(ListView):
+    model = TravelItinerary
+    template_name = 'itineraries/index.html'
+    context_object_name = 'itineraries'
+    
+    # set the queryset to return only the itineraries for the currently logged in user
+    def get_queryset(self): 
+        # Get the user from the URL parameter or session
+        user = self.request.user
+        
+        # Filter items based on the user
+        queryset = Item.objects.filter(user=user)
+        return queryset
