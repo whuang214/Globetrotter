0bf8dfbb866728d2f99485bbeeb383bb19fd81cd
whuang214 <whuang2146@gmail.com>
2023-08-21 12:15:56 -0400

    added signup, login view and stub html page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("accounts/signup/", views.SignUpView.as_view(), name="signup"),
+    path("accounts/login/", views.LoginView.as_view(), name="login"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+# models imports
+from .models import TravelItinerary
+
+# auth imports
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
+
+# GET request gets the view
+# POST request submits the form
+class SignUpView(CreateView):
+    template_name = "auth/auth_form.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("home")
+
+    # add a title to the context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Sign Up"
+        return context
+
+
+# automatically passes in fields from the form
+# fields: username, password
+class LoginView(LoginView):
+    template_name = "auth/auth_form.html"
+    # dont need to specify the form_class b/c it is already specified in the super class
+    success_url = reverse_lazy("home")
+
+    # add a title to the context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Log In"
+        return context
