b68d5813001bd66c53931957530fbe716dc1cada
whuang214 <whuang2146@gmail.com>
2023-08-21 15:34:46 -0400

    added models to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TravelItinerary, Activity, Flight
 
 # Register your models here.
+
+admin.site.register(TravelItinerary)
+admin.site.register(Activity)
+admin.site.register(Flight)
