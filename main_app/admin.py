2e20b6b52ab7a1b47b33f31c3efc7c8c3d8eb211
whuang214 <whuang2146@gmail.com>
2023-08-21 15:39:37 -0400

    removed space in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -4,6 +4,5 @@ from .models import TravelItinerary, Activity, Flight
 # Register your models here.
 
 admin.site.register(TravelItinerary)
-
 admin.site.register(Activity)
 admin.site.register(Flight)
