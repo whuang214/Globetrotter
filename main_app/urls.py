3d336dd5745c32d97a39c674378ca99f8d24fc1f
whuang214 <whuang2146@gmail.com>
2023-08-23 12:10:41 -0400

    fix activities create page

--- main_app/urls.py
+++ main_app/urls.py
@@ -27,7 +27,9 @@ urlpatterns = [
         name="delete_itinerary",
     ),
     path(
-        "itineraries/<int:itinerary_pk>/activities/create", views.ActivityCreate.as_view(), name="create_activity",
+        "itineraries/<int:itinerary_pk>/activities/create",
+        views.ActivityCreate.as_view(),
+        name="create_activity",
     ),
     path(
         "itineraries/<int:itinerary_pk>/activities/<int:activity_pk>/update",
